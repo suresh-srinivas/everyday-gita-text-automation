b6ad6626eab7502daab1fc2ad621356944209b72
sureshsrinivas <30083852+suresh-srinivas@users.noreply.github.com>
2023-09-17 15:21:57 -0700

    This script allows for the automated extraction of verses from the Bhagavad Gita as present on the website shlokam.org.

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # everyday-gita-text-automation
-Automation for Everyday G.I.T.A Text
+Automation for Everyday G.I.T.A Text 
+This script allows for the automated extraction of verses from the Bhagavad Gita as present on the website [shlokam.org](https://shlokam.org). It fetches the verses chapter-wise, and after completing each chapter, a `.txt` file containing the verses of that chapter is downloaded.
+
+## Features
+
+- Batch extraction of verses.
+- Auto-download verses in text format chapter-wise.
+- Configurable to specify which chapter to start with.
+
+## Prerequisites
+
+- A web browser with Developer Tools (like Chrome, Firefox).
+- Access to the website [shlokam.org](https://shlokam.org).
+
+## How to Use
+
+1. **Navigate to the Website:**
+   
+   Open your web browser and visit [shlokam.org](https://shlokam.org).
+
+2. **Open Developer Tools:**
+
+   - **For Chrome**: Press `Ctrl + Shift + J` (Windows/Linux) or `Cmd + Option + J` (Mac).
+   - **For Firefox**: Press `Ctrl + Shift + K` (Windows/Linux) or `Cmd + Option + K` (Mac).
+
+3. **Run the Script:**
+
+   Copy the provided JavaScript script and paste it into the console tab of the Developer Tools and press `Enter`.
+
+4. **Wait for Extraction and Download:**
+
+   The script will start fetching the verses chapter-wise. After completing each chapter, a `.txt` file containing the verses of that chapter will be automatically downloaded. By default, the script starts with Chapter 12, but you can modify it to start from any chapter.
+
+5. **Stopping the Script:**
+
+   If you want to stop the extraction at any point, you can typically do this by closing or refreshing the browser tab.
+
+## Notes
+
+- The extraction might take some time depending on the number of verses and chapters. Ensure you have a stable internet connection.
+- Make sure pop-ups are not blocked in the browser settings, as it might interfere with the downloading of files.
+- If the website structure changes in the future, the script might need modifications to work correctly.
+
+## Credits
+
+- Verses Source: [shlokam.org](https://shlokam.org)
+- Script developed with assistance from OpenAI's ChatGPT.
+
---
+++ gita-text-automation.js
@@ -0,0 +1,104 @@
+const baseUrl = 'https://shlokam.org/bhagavad-gita/';
+
+
+const chapterData = [
+    { chapter: 1, name: 'Arjuna Vishada Yoga', verses: 47 },
+    { chapter: 2, name: 'Sankhya Yoga', verses: 72 },
+    { chapter: 3, name: 'Karma Yoga', verses: 43 },
+    { chapter: 4, name: 'Gyana-Karma-Sanyasa Yoga', verses: 42 },
+    { chapter: 5, name: 'Karma-Sanyasa Yoga', verses: 29 },
+    { chapter: 6, name: 'Atma-Samyama Yoga', verses: 47 },
+    { chapter: 7, name: 'gnana-Vignana Yoga', verses: 30 },
+    { chapter: 8, name: 'Aksara-ParaBrahma Yoga', verses: 28 },
+    { chapter: 9, name: 'Raja-Vidya-Raja-Guhya Yoga', verses: 34 },
+    { chapter: 10, name: 'Vibhuti Yoga', verses: 42 },
+    { chapter: 11, name: 'Vishwarupa-Darsana Yoga', verses: 55 },
+    { chapter: 12, name: 'Bhakti Yoga', verses: 20 },
+    { chapter: 13, name: 'Ksetra-Ksetrajna-Vibhaga Yoga', verses: 34 },
+    { chapter: 14, name: 'Gunatraya-Vibhaga Yoga', verses: 27 },
+    { chapter: 15, name: 'Purushottama Yoga', verses: 20 },
+    { chapter: 16, name: 'Daivasura-Sampad-Vibhaga Yoga', verses: 24 },
+    { chapter: 17, name: 'Shraddhatraya-Vibhaga Yoga', verses: 28 },
+    { chapter: 18, name: 'Moksha-Sanyasa Yoga', verses: 78 }
+];
+
+let currentChapterIndex = chapterData.findIndex(ch => ch.chapter === 12); // Start with Chapter 12
+
+let chapterVerses = [];
+let verseIndex = 1;  // starting from verse 1
+const batchSize = 10;
+
+function extractVerseContent(document) {
+    let paragraphs = document.querySelectorAll('p');
+    let extractedText = '';
+    for (let i = 0; i < 4 && i < paragraphs.length; i++) {
+        extractedText += paragraphs[i].innerText + '\n\n';
+    }
+    return extractedText.trim() || null;
+}
+
+function downloadToFile(content, filename, contentType) {
+    const a = document.createElement('a');
+    const file = new Blob([content], { type: contentType });
+
+    a.href = URL.createObjectURL(file);
+    a.download = filename;
+    a.click();
+
+    URL.revokeObjectURL(a.href);
+}
+
+function fetchSingleVerse(chapter, verseNum) {
+    return fetch(baseUrl + chapter + '-' + verseNum)
+        .then(response => response.text())
+        .then(text => {
+            const parser = new DOMParser();
+            const doc = parser.parseFromString(text, 'text/html');
+            return extractVerseContent(doc);
+        });
+}
+
+function fetchVerse(verseNum) {
+    const currentChapter = chapterData[currentChapterIndex];
+    if (verseNum > currentChapter.verses) {
+        console.log(`Extraction complete for Chapter ${currentChapter.chapter}.`);
+        downloadToFile(chapterVerses.join('\n\n'), `Chapter_${currentChapter.chapter}_Verses.txt`, 'text/plain');
+
+        // Reset chapterVerses and move to next chapter
+        chapterVerses = [];
+        currentChapterIndex++;
+        if (currentChapterIndex < chapterData.length) {
+            setTimeout(() => fetchVerse(1), 3000); // Wait 3 seconds before starting next chapter
+        }
+        return;
+    }
+
+    const fetchPromises = [];
+
+    // Prepare a batch of fetch promises, but not exceeding the lastVerse of current chapter
+    for (let i = 0; i < batchSize && (verseNum + i) <= currentChapter.verses; i++) {
+        fetchPromises.push(fetchSingleVerse(currentChapter.chapter, verseNum + i));
+    }
+
+    Promise.all(fetchPromises)
+        .then(verseContents => {
+            verseContents.forEach((content, index) => {
+                if (content) {
+                    chapterVerses.push(content);
+                    console.log(`Chapter ${currentChapter.chapter}, Verse ${verseNum + index} extracted.`);
+                }
+            });
+
+            // Continue fetching the next batch after a slight delay.
+            setTimeout(() => {
+                fetchVerse(verseNum + batchSize);
+            }, 2000); // 2-second delay. Adjust as needed.
+        })
+        .catch(error => {
+            console.error(`Failed to fetch verses for Chapter ${currentChapter.chapter} starting from ${verseNum}.`, error);
+        });
+}
+
+fetchVerse(verseIndex);
+
+
